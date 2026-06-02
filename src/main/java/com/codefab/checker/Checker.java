2b56e8067bf92392f49752dcfc47e9676ee43fb9
soysoybean22 <soysoybean22@gmail.com>
2026-06-02 16:45:53 +0900

    [feat] 피연산자에 자기 참조 check

--- src/main/java/com/codefab/checker/Checker.java
+++ src/main/java/com/codefab/checker/Checker.java
@@ -93,7 +93,11 @@ public class Checker implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         resolve(expr.right);
         return null;
     }
-    @Override public Void visitUnary(Expr.Unary expr)             { return null; }
+    @Override
+    public Void visitUnary(Expr.Unary expr) {
+        resolve(expr.right);
+        return null;
+    }
     @Override public Void visitLogical(Expr.Logical expr)         { return null; }
     @Override public Void visitGrouping(Expr.Grouping expr)       { return null; }
 }
