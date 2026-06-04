83f4dde93e191e9760d693e1a08955480a2634c8
soysoybean22 <soysoybean22@gmail.com>
2026-06-04 10:28:55 +0900

    [feat] visitUnary/Logical/Grouping - 자식 노드 순회 구현

--- src/main/java/com/codefab/checker/Checker.java
+++ src/main/java/com/codefab/checker/Checker.java
@@ -90,7 +90,22 @@ public class Checker implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         resolve(expr.right);
         return null;
     }
-    @Override public Void visitUnary(Expr.Unary expr)           { return null; }
-    @Override public Void visitLogical(Expr.Logical expr)       { return null; }
-    @Override public Void visitGrouping(Expr.Grouping expr)     { return null; }
+    @Override
+    public Void visitUnary(Expr.Unary expr) {
+        resolve(expr.right);
+        return null;
+    }
+
+    @Override
+    public Void visitLogical(Expr.Logical expr) {
+        resolve(expr.left);
+        resolve(expr.right);
+        return null;
+    }
+
+    @Override
+    public Void visitGrouping(Expr.Grouping expr) {
+        resolve(expr.expression);
+        return null;
+    }
 }
