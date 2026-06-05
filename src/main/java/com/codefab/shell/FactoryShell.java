7f27d430a81a0a6a31ef51c3bb135705cf000dea
soysoybean22 <soysoybean22@gmail.com>
2026-06-05 15:57:32 +0900

    [feat] FactoryShell run 명령 파일 경로 누락 에러 처리 추가

--- src/main/java/com/codefab/shell/FactoryShell.java
+++ src/main/java/com/codefab/shell/FactoryShell.java
@@ -16,6 +16,10 @@ public class FactoryShell {
   public int dispatch(String[] args) throws IOException {
     FileRunner fileRunner = new FileRunner(out, err);
     if (args[0].equals("run")) {
+      if (args.length < 2) {
+        err.println("[오류] 파일 경로가 필요합니다. 사용법: factory run <파일경로>");
+        return 64;
+      }
       return fileRunner.run(args[1]);
     }
     return fileRunner.run(args[0]);
