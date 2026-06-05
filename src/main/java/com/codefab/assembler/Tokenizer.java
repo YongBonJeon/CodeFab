0858e779736854afc827ebdb7e8e6149fb3995dc
soysoybean22 <soysoybean22@gmail.com>
2026-06-05 16:45:52 +0900

    [fix] Tokenizer에 LEFT_BRACKET, RIGHT_BRACKET 토큰 처리 추가

--- src/main/java/com/codefab/assembler/Tokenizer.java
+++ src/main/java/com/codefab/assembler/Tokenizer.java
@@ -55,6 +55,8 @@ public class Tokenizer {
       case ')' -> addToken(RIGHT_PAREN);
       case '{' -> addToken(LEFT_BRACE);
       case '}' -> addToken(RIGHT_BRACE);
+      case '[' -> addToken(LEFT_BRACKET);
+      case ']' -> addToken(RIGHT_BRACKET);
       case ';' -> addToken(SEMICOLON);
       case ',' -> addToken(COMMA);
       case '+' -> addToken(PLUS);
